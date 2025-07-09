47fa58c3a5d6265ff07741093522ca517daa8297
redxfpv <tushdpk@gmail.com>
2025-07-09 22:00:31 +0530

    Initial

---
+++ website/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+def create_app():
+    app=Flask(__name__,template_folder='templates', static_folder='static')
+    app.config['SECRET_KEY'] = 'your_secret_key_here'
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    return app
